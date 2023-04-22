2f0fd584170c0e0c86d8f79a7a7a2da81672cede
holyminer <ryanlxz@gmail.com>
2023-04-22 10:52:48 +0800

    add config file and get_stock_data function

--- src/data.py
+++ src/data.py
@@ -2,12 +2,21 @@ import string
 import pandas as pd
 import cloudscraper
 from bs4 import BeautifulSoup
+import yfinance as yf
 
-def scrape_stock_symbols():
+def scrape_stock_symbols(cfg:dict):
+    """Scrape stock ticker symbols from the new york stock exchange 
+    'https://randerson112358.medium.com/web-scraping-stock-tickers-using-python-3e5801a52c6d'
+    Args:
+        cfg (dict): config datapipeline dictionary
+
+    Returns:
+        pd.DataFrame: Pandas dataframe of company names and their tickers 
+    """
     company_name =[]
     company_ticker = []
     for letter in string.ascii_uppercase:
-        URL =  'https://www.advfn.com/nyse/newyorkstockexchange.asp?companies='+letter
+        URL =  cfg['URL']+letter
         scraper = cloudscraper.create_scraper()
         info = scraper.get(URL).text
         soup = BeautifulSoup(info, "html.parser")
@@ -21,8 +30,16 @@ def scrape_stock_symbols():
             row = i.find_all('td')
             company_name.append(row[0].text.strip())
             company_ticker.append(row[1].text.strip())
-    data = pd.DataFrame(columns = ['company_name',  'company_ticker']) 
-    data['company_name'] = company_name
-    data['company_ticker'] = company_ticker
-    data = data[data['company_name'] != '']
-    return data
+    ticker_df = pd.DataFrame(columns = ['company_name',  'company_ticker']) 
+    ticker_df['company_name'] = company_name
+    ticker_df['company_ticker'] = company_ticker
+    ticker_df = ticker_df[ticker_df['company_name'] != '']
+    return ticker_df
+
+def get_stock_data(cfg:dict):
+    ticker_df = scrape_stock_symbols()
+    stock_data = pd.DataFrame()
+    for i in ticker_df['company_ticker']:
+        stock = yf.download(i, start=cfg['start_date'], end=cfg['end_date'], interval=cfg['interval'])
+        stock = stock.dropna().reset_index() 
+        nyse_dict[i] = security_price
