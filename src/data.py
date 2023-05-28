1eadfc114ab263b069a6a631d7d0f104470689c1
Ryanlxz <ryanlxz@gmail.com>
2023-05-28 22:53:27 +0800

    implement MACD class and get exit position

--- src/data.py
+++ src/data.py
@@ -4,18 +4,20 @@ import cloudscraper
 from bs4 import BeautifulSoup
 import yfinance as yf
 import pathlib
+import os
 from os.path import exists
 
-def load_preprocessed_data(cfg:dict)->pd.DataFrame:
+
+def load_preprocessed_data(cfg: dict) -> pd.DataFrame:
     """load preprocessed data
 
     Args:
         cfg (dict): config dictionary
 
     Returns:
-        pd.DataFrame: preprocessed dataframe 
+        pd.DataFrame: preprocessed dataframe
     """
-    datapath = pathlib.Path(cfg['preprocessed_datapath'])
+    datapath = pathlib.Path(cfg["preprocessed_datapath"])
 
     if exists(datapath):
         stock_data = pd.read_csv(datapath)
@@ -27,136 +29,146 @@ def load_preprocessed_data(cfg:dict)->pd.DataFrame:
         stock_data.to_csv(datapath)
     return stock_data
 
-def load_close_data(cfg:dict)->pd.DataFrame:
+
+def load_close_data(cfg: dict) -> pd.DataFrame:
     """load Close data
 
     Args:
         cfg (dict): config dictionary
 
     Returns:
-        pd.DataFrame: dataframe with Close price 
+        pd.DataFrame: dataframe with Close price
     """
-    datapath = pathlib.Path(cfg['close_datapath'])
+    datapath = pathlib.Path(cfg["close_datapath"])
 
     if exists(datapath):
         df = pd.read_csv(datapath)
     else:
         stock_data = load_preprocessed_data(cfg)
-        df = stock_data['Close']
-        df.index = stock_data['Date'].iloc[:,0]
-        df.index = df.index.rename('Date')
+        df = stock_data["Close"]
+        df.index = stock_data["Date"].iloc[:, 0]
+        df.index = df.index.rename("Date")
         df.rename(columns=lambda x: str(x) + "_Close", inplace=True)
         df.to_csv(datapath)
     return df
 
 
-def scrape_stock_symbols(cfg:dict):
-    """Scrape stock ticker symbols from the new york stock exchange 
+def scrape_stock_symbols(cfg: dict):
+    """Scrape stock ticker symbols from the new york stock exchange
     'https://randerson112358.medium.com/web-scraping-stock-tickers-using-python-3e5801a52c6d'
     Args:
         cfg (dict): config datapipeline dictionary
         URL: website to scrape stock tickers from
 
     Returns:
-        pd.DataFrame: Pandas dataframe of company names and their tickers 
+        pd.DataFrame: Pandas dataframe of company names and their tickers
     """
-    company_name =[]
+    company_name = []
     company_ticker = []
     for letter in string.ascii_uppercase:
-        URL =  cfg['URL']+letter
+        URL = cfg["URL"] + letter
         scraper = cloudscraper.create_scraper()
         info = scraper.get(URL).text
         soup = BeautifulSoup(info, "html.parser")
-        odd_rows = soup.find_all('tr', attrs= {'class':'ts0'})
-        even_rows = soup.find_all('tr', attrs= {'class':'ts1'})
+        odd_rows = soup.find_all("tr", attrs={"class": "ts0"})
+        even_rows = soup.find_all("tr", attrs={"class": "ts1"})
         for i in odd_rows:
-            row = i.find_all('td')
+            row = i.find_all("td")
             company_name.append(row[0].text.strip())
             company_ticker.append(row[1].text.strip())
         for i in even_rows:
-            row = i.find_all('td')
+            row = i.find_all("td")
             company_name.append(row[0].text.strip())
             company_ticker.append(row[1].text.strip())
-    ticker_df = pd.DataFrame(columns = ['company_name',  'company_ticker']) 
-    ticker_df['company_name'] = company_name
-    ticker_df['company_ticker'] = company_ticker
-    ticker_df = ticker_df[ticker_df['company_name'] != '']
+    ticker_df = pd.DataFrame(columns=["company_name", "company_ticker"])
+    ticker_df["company_name"] = company_name
+    ticker_df["company_ticker"] = company_ticker
+    ticker_df = ticker_df[ticker_df["company_name"] != ""]
     return ticker_df
 
-def get_stock_data(cfg:dict):
-    """Download stock data for the scraped stock tickers 
+
+def get_stock_data(cfg: dict):
+    """Download stock data for the scraped stock tickers
 
     Args:
-        cfg (dict): config datapipeline dictionary 
+        cfg (dict): config datapipeline dictionary
         start_date: start date of stock data
         end_date: end date of stock data
         interval: time interval of stock data e.g 1D, 1H
         datapath: data path of stock data
 
     Returns:
-        pd.DataFrame: Pandas dataframe of stock data sorted by their tickers 
+        pd.DataFrame: Pandas dataframe of stock data sorted by their tickers
     """
     datapath = pathlib.Path(cfg["raw_datapath"])
 
     if exists(datapath):
         stock_data = pd.read_csv(datapath)
     else:
+        os.makedirs("../data")
         ticker_df = scrape_stock_symbols(cfg)
         stock_data = pd.DataFrame()
-        for i in ticker_df['company_ticker']:
-            stock = yf.download(i, start=cfg['start_date'], end=cfg['end_date'], interval=cfg['interval'])
-            stock = stock.dropna().reset_index() 
-            stock['symbol'] = i
-            stock_data = pd.concat([stock_data,stock])
-        stock_data = stock_data.pivot(columns='symbol')
-        stock_data.to_csv(cfg['datapath'])
+        for i in ticker_df["company_ticker"]:
+            stock = yf.download(
+                i,
+                start=cfg["start_date"],
+                end=cfg["end_date"],
+                interval=cfg["interval"],
+            )
+            stock = stock.dropna().reset_index()
+            stock["symbol"] = i
+            stock_data = pd.concat([stock_data, stock])
+        stock_data = stock_data.pivot(columns="symbol")
+        stock_data.to_csv(datapath)
     return stock_data
 
-def convert_multiindex(stock_data:pd.DataFrame)->pd.DataFrame:
-    """convert dataframe into a multiindex 
+
+def convert_multiindex(stock_data: pd.DataFrame) -> pd.DataFrame:
+    """convert dataframe into a multiindex
 
     Args:
         stock_data (pd.DataFrame): raw dataframe
 
     Returns:
-        pd.DataFrame: multiindex 
+        pd.DataFrame: multiindex
     """
-    # check if all tickers are the same for all columns 
+    # check if all tickers are the same for all columns
     test_df = pd.DataFrame()
-    for i in ['^Date','^Open','^High','^Low','^Close','^Adj Close','^Volume']:
-        test_df = pd.concat([test_df,stock_data.filter(regex=i).iloc[0].reset_index(drop=True)],axis=1)
-    test_df['matching'] = test_df.eq(test_df.iloc[:, 0], axis=0).all(1)
-    assert not False in test_df['matching'], 'Tickers do not match in all columns'
+    for i in ["^Date", "^Open", "^High", "^Low", "^Close", "^Adj Close", "^Volume"]:
+        test_df = pd.concat(
+            [test_df, stock_data.filter(regex=i).iloc[0].reset_index(drop=True)], axis=1
+        )
+    test_df["matching"] = test_df.eq(test_df.iloc[:, 0], axis=0).all(1)
+    assert not False in test_df["matching"], "Tickers do not match in all columns"
 
-    # configure headers 
-    stock_data.drop(['Unnamed: 0'], axis=1,inplace=True)
-    header_cols = ['Date','Open','High','Low','Close','Adj Close','Volume']
-    length = len(stock_data.filter(like='Date').columns.tolist())
-    tickers = stock_data.filter(like='Date').iloc[0].tolist()
+    # configure headers
+    stock_data.drop(["Unnamed: 0"], axis=1, inplace=True)
+    header_cols = ["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]
+    length = len(stock_data.filter(like="Date").columns.tolist())
+    tickers = stock_data.filter(like="Date").iloc[0].tolist()
     header_1 = []
-    for i in [length*[i] for i in header_cols]:
+    for i in [length * [i] for i in header_cols]:
         for j in i:
             header_1.append(j)
-    header = [header_1, 
-            len(header_cols)*tickers]
-    stock_data.columns=header
-    stock_data = stock_data.iloc[1:].reset_index(drop=True) 
-    return stock_data 
+    header = [header_1, len(header_cols) * tickers]
+    stock_data.columns = header
+    stock_data = stock_data.iloc[1:].reset_index(drop=True)
+    return stock_data
 
-def drop_tickers(stock_data:pd.DataFrame)->pd.DataFrame:
-    """Drop tickers with more than 10 years of missing data. 
+
+def drop_tickers(stock_data: pd.DataFrame) -> pd.DataFrame:
+    """Drop tickers with more than 10 years of missing data.
     252 trading days in a year so drop rows with >= 2520 missing values
 
     Args:
         stock_data (pd.DataFrame): dataframe
 
     Returns:
-        pd.DataFrame: dataframe after dropping tickers with >=10 years of missing values 
+        pd.DataFrame: dataframe after dropping tickers with >=10 years of missing values
     """
     drop_ticker_list = []
-    for i in stock_data['Date'].columns.tolist():
-        if stock_data['Date'][i].isnull().sum() >= 2520:
+    for i in stock_data["Date"].columns.tolist():
+        if stock_data["Date"][i].isnull().sum() >= 2520:
             drop_ticker_list.append(i)
-    stock_data = stock_data.drop(drop_ticker_list, axis=1, level = 1)
+    stock_data = stock_data.drop(drop_ticker_list, axis=1, level=1)
     return stock_data
-
--- src/strategies.py
+++ src/strategies.py
@@ -3,54 +3,120 @@ import numpy as np
 import pandas as pd
 
 
-def profit():
-    profit_macd_df = pd.DataFrame(columns=["profit", "win_rate"])
+class MACD:
+    """MACD strategy which involves getting the entry and exit trade positions"""
 
+    def __init__(self, stock_data):
+        self.stock_data = stock_data
 
-def MACD(ticker: pd.Series) -> pd.Series:
-    """MACD strategy which will be applied to stock dataframe
+    def apply(self):
+        entry_position = self.stock_data.iloc[:, 1:].apply(lambda x: self.macd_entry(x))
+        # entry_position["Date"] = self.stock_data["Date"]
+        entry_position.rename(
+            columns=lambda x: x.replace("_Close", "_Entry"), inplace=True
+        )
+        exit_position = self.stock_data.iloc[:, 1:].apply(lambda x: self.rsi_exit(x))
+        exit_position.rename(
+            columns=lambda x: x.replace("_Close", "_Exit"), inplace=True
+        )
+        positions = pd.concat([entry_position, exit_position], axis=1)
+        positions = positions.reindex(columns=sorted(positions.columns))
+        return positions
 
-    Args:
-        ticker (pd.Series): Close price of ticker
+    def get_exit_position(
+        self, ticker_enter: pd.Series, ticker_exit: pd.Series
+    ) -> pd.Series:
+        """get exit positions for each entry position
 
-    Returns:
-        pd.DataFrame: Dataframe with trading positions
-    """
-    # Initialize macd histogram
-    macd_histogram = talib.MACD(ticker, fastperiod=12, slowperiod=26, signalperiod=9)
-    macd_df = pd.DataFrame()
-    macd_df["macd_histogram"] = macd_histogram[2]
+        Args:
+            ticker_enter (pd.Series): entry positions
+            ticker_exit (pd.Series): exit signals
 
-    # # entry
-    macd_df["long_signal"] = np.where(
-        (macd_df["macd_histogram"] > 0) & (macd_df["macd_histogram"].shift(1) <= 0),
-        1,
-        0,
-    )
-    macd_df["short_signal"] = np.where(
-        (macd_df["macd_histogram"] < 0) & (macd_df["macd_histogram"].shift(1) >= 0),
-        1,
-        0,
-    )
-    macd_df["long_position"] = np.where(macd_df["long_signal"].shift(1) == 1, 1, 0)
-    macd_df["short_position"] = np.where(macd_df["short_signal"].shift(1) == 1, 1, 0)
-    position = np.where(
-        macd_df["long_position"] == 1,
-        "long",
-        np.where(macd_df["short_position"] == 1, "short", np.nan),
-    )
-    return position
+        Returns:
+            pd.Series: index of exit position for each entry position
+        """
+        exit_list = []
+        # col_name = f'{ticker_enter.name.split("_")[0]}_Exit_Position'
+        entry_idx = ticker_enter[ticker_enter == "enter_long"].index
+        for i in entry_idx:
+            if (
+                len(ticker_exit[(ticker_exit.index > i) & (ticker_exit == "exit_long")])
+                > 0
+            ):
+                # case 1: entry and exit positions are available
+                first_occurrence_index = ticker_exit[
+                    (ticker_exit.index > i) & (ticker_exit == "exit_long")
+                ].index[0]
+                exit_list.append(first_occurrence_index)
+            elif len(ticker_exit[(ticker_exit.index > i)]) == 0:
+                # case 2: enter trade on the last date of the dataframe so not possible to exit
+                exit_list.append("exit_na")
+            else:
+                # case 3: no exit position after entry position. Exit at last available date.
+                exit_list.append(ticker_exit.loc[i:].iloc[-1])
+        exit_idx = pd.Series(exit_list)
+        return exit_idx
 
-    # # exit (2-day RSI of single day is greater than 65 for long positions, and less than 35 for short positions)
-    # macd_df["rsi"] = talib.RSI(macd_df["Close"], timeperiod=2)
-    # macd_df["entry_date"] = ""
-    # for i in macd_df.loc[macd_df["position"] == 1].index:
-    #     macd_df["entry_date"].iloc[i] = macd_df["Date"].iloc[i]
-    # macd_df["exit_long"] = np.where(
-    #     macd_df["rsi"].shift(1) > 65, 1, 0
-    # )  # signal to exit is generated when 2-period RSI above 65, but exit trade is only executed on following day.
-    # macd_df["exit_short"] = np.where(macd_df["rsi"].shift(1) < 35, 1, 0)
-    # macd_df["exit_date"] = ""
+    def macd_entry(self, ticker: pd.Series) -> pd.Series:
+        """MACD entry strategy which will be applied to stock dataframe
+
+        Args:
+            ticker (pd.Series): Close price of ticker
+
+        Returns:
+            pd.Series: Series with entry positions
+        """
+        # Initialize macd histogram
+        macd_histogram = talib.MACD(
+            ticker, fastperiod=12, slowperiod=26, signalperiod=9
+        )
+        macd_df = pd.DataFrame()
+        macd_df["macd_histogram"] = macd_histogram[2]
+
+        # entry
+        macd_df["long_signal"] = np.where(
+            (macd_df["macd_histogram"] > 0) & (macd_df["macd_histogram"].shift(1) <= 0),
+            1,
+            0,
+        )
+        macd_df["short_signal"] = np.where(
+            (macd_df["macd_histogram"] < 0) & (macd_df["macd_histogram"].shift(1) >= 0),
+            1,
+            0,
+        )
+        macd_df["long_position"] = np.where(macd_df["long_signal"].shift(1) == 1, 1, 0)
+        macd_df["short_position"] = np.where(
+            macd_df["short_signal"].shift(1) == 1, 1, 0
+        )
+        entry_position = np.where(
+            macd_df["long_position"] == 1,
+            "enter_long",
+            np.where(macd_df["short_position"] == 1, "enter_short", np.nan),
+        )
+        return entry_position
+
+    def rsi_exit(self, ticker: pd.Series) -> pd.Series:
+        """RSI exit strategy
+        2-day RSI of single day is greater than 65 for long positions, and less than 35 for short positions
+
+        Args:
+            ticker (pd.Series): Close price of ticker
+
+        Returns:
+            pd.Series: Series with exit positions
+        """
+        exit_df = pd.DataFrame()
+        exit_df["rsi"] = talib.RSI(ticker, timeperiod=2)
+        # signal to exit is generated when 2-period RSI above 65, but exit trade is only executed on the following day.
+        exit_df["exit_long"] = np.where(exit_df["rsi"].shift(1) > 65, 1, 0)
+        exit_df["exit_short"] = np.where(exit_df["rsi"].shift(1) < 35, 1, 0)
+        exit_position = np.where(
+            exit_df["exit_long"] == 1,
+            "exit_long",
+            np.where(exit_df["exit_short"] == 1, "exit_short", np.nan),
+        )
+        return exit_position
+
     # # exit long positions
     # for i in macd_df.loc[macd_df["long_position"] == 1].index:
     #     new_macd_df = macd_df.loc[i + 2 :]
@@ -114,12 +180,10 @@ def MACD(ticker: pd.Series) -> pd.Series:
     # return macd_df[macd_df["position"] == 1]
 
 
-class exit:
+def profit():
+    profit_macd_df = pd.DataFrame(columns=["profit", "win_rate"])
+
+
+def close_exit():
+    # stop loss exit. when close price falls below or exceeds a threshold, generate exit signal
     pass
-
-    def rsi():
-        pass
-
-    def profit():
-        pass
-        # a certain profit has been reached e.g 10%
