05e73847861f5891b5bad2d834a212de3b638385
holyminer <ryanlxz@gmail.com>
2023-07-26 21:08:51 +0800

    added profit_percent and profit_absolute for profit class, and tested stock trend following system in notebook

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from datetime import datetime
 
+def get_column_name(dataframe:pd.DataFrame,suffix:str)->str:
+        """helper function to get column name which ends with the specified suffix
+
+        Args:
+            dataframe (pd.DataFrame): dataframe which contains the needed column name 
+            suffix (str): ending of column name which follows the format: _suffix. e.g _Exit
+
+        Returns:
+            str: column name
+        """
+        return dataframe.columns[dataframe.columns.str.endswith(suffix)][0]
+
 class Profit:
     def __init__(self, positions_df: pd.DataFrame, price_df: pd.DataFrame):
         """Calculate profit for both long and short strategies 
@@ -13,18 +25,6 @@ class Profit:
         self.positions_df = positions_df
         self.price_df = price_df
     
-    def get_column_name(self, dataframe:pd.DataFrame,suffix:str)->str:
-        """get column name which ends with the specified suffix
-
-        Args:
-            dataframe (pd.DataFrame): dataframe which contains the needed column name 
-            suffix (str): ending of column name which follows the format: _suffix. e.g _Exit
-
-        Returns:
-            str: column name
-        """
-        return dataframe.columns[dataframe.columns.str.endswith(suffix)][0]
-
     def calculate_profit(self,position:str):
         """calculate trading profits. Merge the positions and price_df to get the Close price for entry and exit dates, and calculate the profit according to the trade position. 
 
@@ -37,24 +37,26 @@ class Profit:
         # check if position input is in the correct format
         if position not in ['long','short']:
             print('Position is an incorrect value. It has to be either long or short.')
-        self.entry_column = self.get_column_name(self.positions_df,'_Entry')
-        self.exit_column = self.get_column_name(self.positions_df,'_Exit')
+        self.entry_column = get_column_name(self.positions_df,'_Entry')
+        self.exit_column = get_column_name(self.positions_df,'_Exit')
         #merge positions and price df to get the Close price for entry and exit dates
         merged_df = pd.merge(self.positions_df, self.price_df, left_on=self.entry_column, right_on='Date', how='left')
         merged_df = pd.merge(merged_df, self.price_df, left_on=self.exit_column, right_on='Date', suffixes=('_Entry', '_Exit'), how='left')
         # drop unnecessary columns
         merged_df.drop(['Date_Entry','Date_Exit'],axis=1,inplace=True)
         # calculate profits
-        entry_price = self.get_column_name(merged_df,'Close_Entry')
-        exit_price = self.get_column_name(merged_df,'Close_Exit')
+        entry_price = get_column_name(merged_df,'Close_Entry')
+        exit_price = get_column_name(merged_df,'Close_Exit')
         if position == 'long':
-            merged_df['profit'] = (merged_df[exit_price]-merged_df[entry_price])/merged_df[entry_price] + 1
+            merged_df['profit_percent'] = (merged_df[exit_price]-merged_df[entry_price])/merged_df[entry_price] + 1
+            merged_df['profit_absolute'] = (merged_df[exit_price]-merged_df[entry_price])
 
         elif position == 'short':
-            merged_df['profit'] = (merged_df[entry_price]-merged_df[exit_price])/merged_df[entry_price] + 1
+            merged_df['profit_percent'] = (merged_df[entry_price]-merged_df[exit_price])/merged_df[entry_price] + 1
+            merged_df['profit_absolute'] = (merged_df[entry_price]-merged_df[exit_price])
         return merged_df
     
-    def calculate_long_term_profit(self,profit_df:pd.DataFrame, start_year:int=0, end_year:int=9999)->float:
+    def calculate_long_term_profit(self,profit_df:pd.DataFrame, profit_type:str = 'cumulative',start_year:int=0, end_year:int=9999)->float:
         """calculate long term profit over the year range. Year range defaults to 1900-2200 which is the entire duration of the dataframe. 
 
         Args:
@@ -70,4 +72,10 @@ class Profit:
         profit_df[self.exit_column] = pd.to_datetime(profit_df[self.exit_column])
         profit_df['entry_year'] = profit_df[self.entry_column].dt.year
         profit_df['exit_year'] = profit_df[self.exit_column].dt.year
-        return profit_df.loc[(profit_df['entry_year'].between(year_range[0], year_range[1])) & (profit_df['exit_year'].between(year_range[0], year_range[1])), 'profit'].prod()
+        if profit_type == 'cumulative':
+            return profit_df.loc[(profit_df['entry_year'].between(year_range[0], year_range[1])) & (profit_df['exit_year'].between(year_range[0], year_range[1])), 'profit_percent'].prod()
+        elif profit_type == 'sum':
+            return profit_df.loc[(profit_df['entry_year'].between(year_range[0], year_range[1])) & (profit_df['exit_year'].between(year_range[0], year_range[1])), 'profit_absolute'].sum()
+        else:
+            print('profit_type is not the right value')
+
--- src/stock_trend_following_system.py
+++ src/stock_trend_following_system.py
@@ -8,6 +8,7 @@ import talib
 import numpy as np
 import pandas as pd
 import sys
+from helper import get_column_name
 
 sys.path.append("../")
 import conf
@@ -18,30 +19,38 @@ UPTREND_MA = conf.backtest_conf["uptrend_ma"] # 100 day MA
 
 class StockTrend:
     def __init__(self, stock_df: pd.DataFrame):
+        """Initialize with stock df containing 3 columns: Date, Close and Low prices. 
+
+        Args:
+            stock_df (pd.DataFrame): stock df containing 3 columns: Date, Close and Low prices.
+        """
         self.stock_df = stock_df
+        self.close_col = get_column_name(stock_df,'_Close')
+        self.low_col = get_column_name(stock_df,'_Low')
 
     def generate_buy_dates(self):
-        self.stock_df['100ma'] = talib.SMA(self.stock_df['Close'],timeperiod=UPTREND_MA) 
+        self.stock_df['100ma'] = talib.SMA(self.stock_df[self.close_col],timeperiod=UPTREND_MA) 
         # 1: close price above 100MA, 0: close price below 100MA
-        self.stock_df['above_ma'] = np.where(self.stock_df['Close']>=self.stock_df['100ma'],1,0) 
+        self.stock_df['above_ma'] = np.where(self.stock_df[self.close_col]>=self.stock_df['100ma'],1,0) 
         # 1 = uptrend if past 15/20 days above 100MA
         self.stock_df['uptrend'] = np.where(self.stock_df['above_ma'].rolling(20).sum()>=15,1,0) 
         # 1 = 10-day low
-        self.stock_df['10_day_low'] = np.where((self.stock_df['uptrend']==1) & (self.stock_df['Low']==self.stock_df['Low'].rolling(10).min()),1,0)
-        # Buy within 3 days on the following day, after 10-day low at 3% limit buy order
-        self.stock_df['entry'] = np.where((self.stock_df['10_day_low'].shift(1)==1) & (self.stock_df['Low'].shift(-2).rolling(3).min()<= 0.97*self.stock_df['Close'].shift(1)),1,0)
+        self.stock_df['10_day_low'] = np.where((self.stock_df['uptrend']==1) & (self.stock_df[self.low_col]==self.stock_df[self.low_col].rolling(10).min()),1,0)
+        # Buy within 3 days on the following day, after 10-day low at 3% limit buy order. Not necessary for price to drop to 3% below 10-day low, so the 3 days is the window to buy if the price drops to 3% below 10-day low. 
+        self.stock_df['entry'] = np.where((self.stock_df['10_day_low'].shift(1)==1) & (self.stock_df[self.low_col].shift(-2).rolling(3).min()<= 0.97*self.stock_df[self.close_col].shift(1)),1,0)
         self.stock_df['entry_date'] = ''
         for i in self.stock_df.loc[self.stock_df['entry']==1].index:
-            if self.stock_df['Low'].iloc[i] <= 0.97*self.stock_df['Close'].iloc[i-1]:
+            if self.stock_df[self.low_col].iloc[i] <= 0.97*self.stock_df[self.close_col].iloc[i-1]:
                 self.stock_df['entry_date'].iloc[i]=self.stock_df['Date'].iloc[i]
-            elif self.stock_df['Low'].iloc[i+1] <= 0.97*self.stock_df['Close'].iloc[i-1]:
+            elif self.stock_df[self.low_col].iloc[i+1] <= 0.97*self.stock_df[self.close_col].iloc[i-1]:
                 self.stock_df['entry_date'].iloc[i]=self.stock_df['Date'].iloc[i+1]
-            elif self.stock_df['Low'].iloc[i+2] <= 0.97*self.stock_df['Close'].iloc[i-1]:
+            elif self.stock_df[self.low_col].iloc[i+2] <= 0.97*self.stock_df[self.close_col].iloc[i-1]:
                 self.stock_df['entry_date'].iloc[i]=self.stock_df['Date'].iloc[i+2] 
+        return self.stock_df
 
     ### Exit
     def generate_sell_dates(self):
-        self.stock_df['rsi'] = talib.RSI(self.stock_df['Close'],timeperiod=2) #2period rsi 
+        self.stock_df['rsi'] = talib.RSI(self.stock_df[self.close_col],timeperiod=2) #2period rsi 
         self.stock_df['exit_date'] = ''
         for i in self.stock_df.loc[self.stock_df['entry_date']!=''].index:
             day = 1
@@ -61,3 +70,4 @@ class StockTrend:
                     self.stock_df['exit_date'].iloc[i] = self.stock_df['Date'].iloc[date+5]
                 except: #Index out of bounds;use last date in dataset to exit
                     self.stock_df['exit_date'].iloc[i] = self.stock_df['Date'].iloc[-1]
+        return self.stock_df
