26e4dbe8407fd412dc6ed7b0d70e5d16cc55d65c
holyminer <ryanlxz@gmail.com>
2023-08-21 22:52:25 +0800

    moved constant parameters from config file to constants.py file

--- conf/constants.py
+++ conf/constants.py
@@ -1,2 +1,7 @@
-# file to store all constants (things that the user is unlikely to change)
+# data 
+raw_data_file_path = "../data/01_raw/stock_data.csv"
+raw_data_tickers_file_path = "../data/01_raw/tickers.json"
+raw_data_url = 'https://www.advfn.com/nyse/newyorkstockexchange.asp?companies='
+preprocessed_data_file_path = "../data/02_preprocessed/new_preprocessed_data.csv"
+# strategies 
 strategy_list = ['macd','stock_trend']
